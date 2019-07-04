55de7614c26d2a6fe47030de492d1d6d4e8478d4
Alice Scholze <alicescholze@outlook.com>
2019-07-03 21:07:44 -0300

    Filtrados dados recebidos, calculado IAT e exibidos por ordem de IAT

--- readReport.py
+++ readReport.py
@@ -1,6 +1,55 @@
 import dpkt
+import socket
 
-f = open ('smallFlows.pcap', 'rb')
-pcap = dpkt.pcap.Reader(f)
-for ts, buf in pcap:
-    print (ts, len(buf))
+class Pcap:
+	ts = None #timeStamp
+	ipOrigem = None
+	ipDestino = None
+	portaDestino = None
+	portaOrigem = None
+	quantRequisicao = 0
+	iatTotal = None
+	iat = None
+
+dadosPcap = []
+
+def readPcap():
+    f = open('smallFlows.pcap', 'rb')
+    pcap = dpkt.pcap.Reader(f)
+    for ts, buf in pcap:
+        eth = dpkt.ethernet.Ethernet(buf)
+        file = Pcap()
+        ip = eth.data
+        tcp = ip.data
+        if eth.type == dpkt.ethernet.ETH_TYPE_IP and ip.p == dpkt.ip.IP_PROTO_TCP:
+            file.ipOrigem = socket.inet_ntoa(ip.src)
+            file.ipDestino = socket.inet_ntoa(ip.dst)
+            file.portaOrigem = tcp.sport
+            file.portaDestino = tcp.dport
+            file.ts = ts
+            file.iat = 0
+            file.iatTotal = 0
+            file.quantRequisicao += 1
+        if file.ipOrigem != None and file.ipDestino != None:
+            for x in dadosPcap:
+                if x.ipOrigem == file.ipOrigem and x.ipDestino == file.ipDestino and x.portaOrigem == file.portaOrigem and x.portaDestino == file.portaDestino:
+                    #Calculo IAT
+                    #soma dos tempos entre as requisições dividido pelo nímero de requisições
+                    x.quantRequisicao += 1
+                    x.iatTotal += file.ts - x.ts
+                    x.iat = x.iatTotal / x.quantRequisicao
+                    #print("Ja existe : ", x.iat, " ip : ", x.ipOrigem)
+                    break
+            else:
+                dadosPcap.append(file)
+                #print("Não existe : ", file.ipOrigem)
+        #print(dadosPcap[y].ipOrigem, " - ", dadosPcap[y].ipDestino, " - ", dadosPcap[y].ts)
+
+def list():
+    dadosPcap.sort(key=lambda x: x.iat, reverse=False)
+    for x in dadosPcap:
+        print("Ip origem: ", x.ipOrigem,"\nPorta origem: ", x.portaOrigem, "\nIp destino: ", x.ipDestino, "\nPorta destino: ", x.portaDestino, "\nIAT: ", x.iat, "\nSoma dos tempos entre as requisições: ", x.iatTotal, "\nQuantidade de requisiçõs: ", x.quantRequisicao)
+        print("*--------------------------------------------------------------*")
+
+readPcap()
+list()
